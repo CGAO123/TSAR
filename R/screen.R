4260e714422b2a9cabadd2bf85aba4f0205ac07a
CGAO123 <candygao2015@outlook.com>
2023-06-22 17:14:36 -0400

    /

--- R/screen.R
+++ R/screen.R
@@ -72,9 +72,13 @@ screen <- function(raw_data,
     ggplot(data = screened, aes(x = Temperature,
                                 y = Fluorescence,
                                 color = Well.Position)) +
+<<<<<<< HEAD
         geom_line(size = 0.2) +
         theme_bw() +
         theme(panel.grid.major = element_blank())
+=======
+        geom_line(size = 0.2)
+>>>>>>> 3aa823a (//)
 
 
 }
