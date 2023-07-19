05a7b39ac06406bf585cea18824b49ce29e746ed
CGAO123 <candygao2015@outlook.com>
2023-07-19 17:18:21 -0400

    restructured weed_raw app to fit bioconductor format

---
+++ R/interface_weed.R
@@ -0,0 +1,135 @@
+mainpage <- function(raw_data) {
+    fluidPage(
+        useShinyjs(),
+        tags$style(
+            HTML(
+                "
+              .sticky-panel {
+                position: sticky;
+                top: 0;
+                z-index: 100;
+                background-color: white;
+              }
+              "
+            )
+        ),
+        fluidRow(
+            column(
+                width = 4,
+                checkboxInput("dialogueToggle",
+                    "Hide All Hints and Messages",
+                    value = FALSE
+                )
+            ),
+            column(
+                width = 4,
+                actionButton(
+                    "clear",
+                    "Clear all Selected"
+                )
+            )
+        ),
+        fluidRow(
+            column(width = 4, h3("Current Raw Dataset: ")),
+            h3(dataName <- deparse(substitute(raw_data)))
+        ),
+        div(
+            class = "sticky-panel",
+            plotlyOutput("distPlot")
+        ),
+        verbatimTextOutput("info"),
+        fluidRow(
+            column(width = 3, h4("Select by Grid")),
+            column(
+                width = 2,
+                h5(" "),
+                materialSwitch("toggle_button", "Show Grid",
+                    value = TRUE, right = TRUE
+                )
+            )
+        ),
+        div(
+            id = "grid-panel",
+            style = "display: none;",
+            fluidRow(
+                column(
+                    width = 12,
+                    div(
+                        id = "grid-container",
+                        style = "display: grid; grid-template-columns:
+                                repeat(12, 50px); grid-gap: 0px;",
+                        lapply(1:96, function(i) {
+                            row <- ceiling(i / 12)
+                            col <- i %% 12
+                            if (col == 0) col <- 12
+                            col_label <- sprintf("%02d", col)
+                            div(
+                                id = paste0("cell-", i),
+                                class = "grid-cell",
+                                style = "background-color: white;
+                                       height: 30px; cursor: pointer;
+                                       display: flex; align-items:
+                                       center; justify-content: center;
+                                       font-size: 12px;",
+                                paste0(LETTERS[row], col_label)
+                            )
+                        })
+                    )
+                )
+            )
+        ),
+        verbatimTextOutput("highlighted_list"),
+        h4("Copy Selections"),
+        fluidRow(
+            column(
+                width = 2,
+                actionButton("myButton", "Copy Well IDs")
+            ),
+            column(
+                width = 4,
+                actionButton(
+                    "removeCall",
+                    "Copy in remove_raw() Call"
+                )
+            ),
+        ),
+        br(),
+        fluidRow(
+            column(
+                width = 6,
+                h4("Refresh Plot"),
+                fluidRow(
+                    column(
+                        width = 4,
+                        actionButton(
+                            "viewRemovedButton",
+                            "View Selected"
+                        )
+                    ),
+                    column(
+                        width = 2,
+                        actionButton(
+                            "refreshButton",
+                            "Refresh Screening"
+                        )
+                    )
+                )
+            ),
+            column(
+                width = 5,
+                h4("Edit Data"),
+                fluidRow(
+                    column(
+                        width = 2,
+                        actionButton(
+                            "removeButton",
+                            "Remove Selected"
+                        )
+                    )
+                )
+            )
+        ),
+        br(),
+        actionButton("stopButton", "Close Window"),
+    )
+}
